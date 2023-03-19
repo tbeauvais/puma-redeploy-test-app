e4a9ade3ef2e5da31a640f5f080c324bbabee540
tbeauvais <tbeauvais1@gmail.com>
2023-03-19 15:31:23 -0400

    pass branch name to build

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #!/bin/ash
 
-git clone --depth 1 -b "$BRANCH_NAME" --single-branch https://github.com/"$REPO_NAME".git "$ARCHIVE_NAME"
+git clone --depth 1 -b "${BRANCH_NAME}" --single-branch https://github.com/"$REPO_NAME".git "${ARCHIVE_NAME}"
 
 # shellcheck disable=SC2164
-cd "$ARCHIVE_NAME"
+cd "${ARCHIVE_NAME}"
 
 bundle config timeout 30
 bundle config deployment 'true'
 bundle config retry 4
 
-echo 'repo !!!!'
-echo "$REPO_NAME"
+echo "repo !!!! ${REPO_NAME}"
 
 # shellcheck disable=SC2016
-rake build_archive['$ARCHIVE_NAME']
+rake build_archive["${ARCHIVE_NAME}"]
 
-cp ./pkg/"$ARCHIVE_NAME_"*.zip /build/pkg/
+cp ./pkg/"${ARCHIVE_NAME}"_*.zip /build/pkg/
