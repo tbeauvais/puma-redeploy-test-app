4fcf320d8595f83589f07395dce8319b5ce7fd37
Tom Beauvais <tbeauvais1@gmail.com>
2023-03-19 11:36:23 -0400

    Update build workflow (#20)
    * Update build archive workflow
    
    * Update build archive workflow
    
    * Update build archive workflow
    
    * Update build archive workflow
    
    * Update build archive workflow

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-git clone --depth 1 https://github.com/tbeauvais/sinatra-api-base.git
+git clone --depth 1 https://github.com/$REPO_NAME.git
 
 cd sinatra-api-base
 
@@ -8,8 +8,9 @@ bundle config timeout 30
 bundle config deployment 'true'
 bundle config retry 4
 
-bundle
+echo 'repo !!!!'
+echo "$REPO_NAME"
 
-bundle exec rake build_archive
+rake build_archive
 
 cp ./pkg/my_application_0.0.1.zip /build/pkg/my_application_0.0.1.zip
