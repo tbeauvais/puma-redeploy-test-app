0b0f94acb99dd088785ac4de913e11b994dede78
tbeauvais <tbeauvais1@gmail.com>
2023-04-21 10:41:00 -0400

    Bump puma-redeploy gem

--- build.sh
+++ build.sh
@@ -9,8 +9,6 @@ bundle config timeout 30
 bundle config deployment 'true'
 bundle config retry 4
 
-echo "repo !!!! ${REPO_NAME}"
-
 # shellcheck disable=SC2016
 rake build_archive["${ARCHIVE_NAME}"]
 
