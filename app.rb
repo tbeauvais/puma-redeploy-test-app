725c0cc7fceef29cc72fd0a026234eb5ec15bd93
tbeauvais <tbeauvais1@gmail.com>
2023-04-22 11:55:08 -0400

    Add version endpoint

--- app.rb
+++ app.rb
@@ -7,8 +7,14 @@ require 'pry' if %w[development test].include? ENV['RACK_ENV']
 
 # app for testing puma reloader
 class App < Sinatra::Base
+  VERSION = File.read('VERSION').strip
+
   get '/ping' do
     JSON.generate({ message: 'pong' })
+  end
+
+  get '/version' do
+    JSON.generate({ version: VERSION })
   end
 
   # new endpoint for testing redeploy
