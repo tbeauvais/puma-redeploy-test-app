22e6b67d4fcb26514314dd9cfc323ff6b0fb219f
tbeauvais <tbeauvais1@gmail.com>
2023-05-06 15:32:13 -0400

    Add support to buid archive from local source

--- build_local.sh
+++ build_local.sh
@@ -1,12 +1,11 @@
 #!/bin/ash
 
+cd /app
 
 bundle lock --add-platform x86_64-linux-musl
 bundle config timeout 30
 bundle config deployment 'true'
 bundle config retry 4
-
-cd /app
 
 # shellcheck disable=SC2016
 rake build_archive["${ARCHIVE_NAME}"]
